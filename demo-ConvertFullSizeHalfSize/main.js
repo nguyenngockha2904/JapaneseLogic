4bf5ca008a99791feb25742a66f16e2a5586e7aa
GenbaStar_Git-at-363348896715 <khann@mor.com.vn>
2021-05-21 11:04:15 +0700

    update

--- demo-ConvertFullSizeHalfSize/main.js
+++ demo-ConvertFullSizeHalfSize/main.js
@@ -1,5 +1,5 @@
-const  convert =(val)=> {
-     let set= val.split(",");
+const convert = (val) => {
+     let set = val.split(",");
      let charsets = {
           latin: { halfRE: /[!-~]/g, fullRE: /[！-～]/g, delta: 0xFEE0 },
           hangul1: { halfRE: /[ﾡ-ﾾ]/g, fullRE: /[ᆨ-ᇂ]/g, delta: -0xEDF9 },
@@ -24,21 +24,80 @@ const  convert =(val)=> {
           [...set.half][[...set.full].indexOf(c)];
      let re = (set, way) => set[way + "RE"] || new RegExp("[" + set[way] + "]", "g");
      let sets = Object.keys(charsets).map(i => charsets[i]);
-     const toFullWidth = str0 =>
-          sets.reduce((str, set) => str.replace(re(set, "half"), toFull(set)), str0);
-     const toHalfWidth = str0 =>
-          sets.reduce((str, set) => str.replace(re(set, "full"), toHalf(set)), str0);
+     const toFullWidth = str0 => {
+          return sets.reduce((str, set) => {
+               return str.replace(re(set, "half"), toFull(set))
+          }, str0);
+     }
+     const toHalfWidth = str0 => {
+          console.log('toHalfWidt', str0);
+          return sets.reduce((str, set) => {
+               return str.replace(re(set, "full"), toHalf(set))
+          }, str0);
+     }
+     const mapSpecialCharacter = (character) => {
+          let FulltoHalf = {
+               'ガ': 'ｶﾞ',
+               'ギ': 'ｷﾞ',
+               'グ': 'ｸﾞ',
+               'ゲ': 'ｹﾞ',
+               'ゴ': 'ｺﾞ',
+               'ザ': 'ｻﾞ',
+               'ジ': 'ｼﾞ',
+               'ズ': 'ｽﾞ',
+               'ゼ': 'ｾﾞ',
+               'ゾ': 'ｿﾞ',
+               'ダ': 'ﾀﾞ',
+               'ヂ': 'ﾁﾞ',
+               'ッ': 'ｯ',
+               'ヅ': 'ﾂﾞ',
+               'デ': 'ﾃﾞ',
+               'ド': 'ﾄﾞ',
+               'バ': 'ﾊﾞ',
+               'パ': 'ﾊﾟ',
+               'ビ': 'ﾋﾞ',
+               'ピ': 'ﾋﾟ',
+               'ブ': 'ﾌﾞ',
+               'プ': 'ﾌﾟ',
+               'ベ': 'ﾍﾞ',
+               'ペ': 'ﾍﾟ',
+               'ボ': 'ﾎﾞ',
+               'ポ': 'ﾎﾟ',
+               'ヴ': 'ｳﾞ',
+          };
+          if (typeof FulltoHalf[character] === 'undefined') {
+               return character;
+          } else {
+               return FulltoHalf[character];
+          }
+     }
+     const convertToSpecialHalfWidth = (string) => {
+          let characters = string.split("");
+          let halfWidthString = ''
+          characters.forEach(character => {
+               halfWidthString += mapSpecialCharacter(character);
+          });
+          return halfWidthString;
+     }
      return {
-       fullWidth:set.map(toFullWidth)[0],
-       halfWidth:set.map(toFullWidth).map(toHalfWidth)[0],
+          fullWidth: set.map(toFullWidth)[0],
+          halfWidth: convertToSpecialHalfWidth(set.map(toFullWidth).map(toHalfWidth)[0]),
+          // halfWidth:set.map(toFullWidth).map(toHalfWidth)[0]
      }
 }
 
 
+
+
 document.getElementById('btnConvert').addEventListener('click', () => {
      let inputVal = document.getElementById('inpTxt').value;
-     
-  const {fullWidth,halfWidth} =  convert(inputVal);
-     console.log('fullWidth :', fullWidth);
-     console.log('halfWidth :', halfWidth);
+
+     const { fullWidth, halfWidth } = convert(inputVal);
+     // let halfWidth =  convertToHalfWidth(inputVal);
+     // let  fullWidth=convertToFullWidth(inputVal);
+     console.log(` value = ${inputVal}   =>  fullwidth: ${fullWidth}  =>  halfwidth=${halfWidth}`);
+     // let halfWidth =  convertToHalfWidth(inputVal);
+     // let  fullWidth=convertToFullWidth(inputVal);
+     // console.log('convertToHalfWidth(inputVal)',convertToHalfWidth(inputVal));
+     // console.log('convertToFullWidth(inputVal)',convertToFullWidth(inputVal));
 })
